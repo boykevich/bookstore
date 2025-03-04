f30db70480e8acf66d1af26d5d1f469b9e0dd946
Roman Andriichuk <romanandriichuik17@gmail.com>
2025-03-04 21:02:49 +0100

    Added Book record

--- BookStore/BookStore/Components/Models/Book.cs
+++ BookStore/BookStore/Components/Models/Book.cs
@@ -1,21 +1,4 @@
 ﻿namespace BookStore.Components.Models
 {
-    public  record Book
-    {
-        private string _name;
-        public string Name
-        {
-            get
-            {
-                return _name;
-            }
-            set
-            {
-                if (value.Length > 10)
-                {
-                    _name = value;
-                }
-            }
-        }
-    }
+    public record Book(string BookName, List<Author> Authors, DateTime DateOfPublishment, string Genre, string Language);
 }
