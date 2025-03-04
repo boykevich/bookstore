ed3d5a265b349b75ee3099a5030385a0f264e37f
Roman Andriichuk <romanandriichuik17@gmail.com>
2025-03-04 16:19:34 +0100

    Initialized book class

---
+++ BookStore/BookStore/Components/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStore.Components.Models
+{
+    public  record Book
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    _name = value;
+                }
+            }
+        }
+    }
+}
