a1ccc607c98209a6d9bf6152a145f86e58f136da
Roman Andriichuk <romanandriichuik17@gmail.com>
2025-03-07 13:31:50 +0100

    Added line of code, so we can use temporary Harry Potter Books list

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -35,6 +35,7 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+builder.Services.AddSingleton<HarryPotterBooks>();
 
 var app = builder.Build();
 
