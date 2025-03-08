92fa0a7e4a6e7c5feec23c370efbd4844dbca92a
Dmitry Boikevych <xboikev@fi.muni.cz>
2025-03-08 15:52:23 +0100

    Add authors

---
+++ BookStore/BookStore/Migrations/20250308143625_AddAuthors.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AuthorBook",
+                columns: new[] { "AuthorsId", "BooksId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 1, 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 1, 3 });
+        }
+    }
+}
