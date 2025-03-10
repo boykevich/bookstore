3781baba596b03aa6abb916ef8637721b04fa721
Roman Andriichuk <romanandriichuik17@gmail.com>
2025-03-10 15:48:08 +0100

    Added cart to our database

--- BookStore/BookStore/Data/ApplicationDbContext.cs
+++ BookStore/BookStore/Data/ApplicationDbContext.cs
@@ -14,6 +14,8 @@ namespace BookStore.Data
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
