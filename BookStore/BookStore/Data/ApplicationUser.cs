e4366216188f3ccc3119b0b454955997bcb92728
Roman Andriichuk <romanandriichuik17@gmail.com>
2025-03-12 16:09:10 +0100

    Added PurchasedBooks list to user

--- BookStore/BookStore/Data/ApplicationUser.cs
+++ BookStore/BookStore/Data/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using BookStore.Components.Models;
 using Microsoft.AspNetCore.Identity;
 
 namespace BookStore.Data;
@@ -5,4 +6,5 @@ namespace BookStore.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public ICollection<PurchasedBook> PurchasedBooks { get; set; } = new List<PurchasedBook>();
 }
